a68bff0a374cc27843583811df66d8d42a2ac4af
Zoubair Entifi <zoentifi@e1r10p4.1337.ma>
2026-09-24 23:35:02 +0100

    3yiiit

--- backend/tests/test_ingestion.py
+++ backend/tests/test_ingestion.py
@@ -1,10 +1,35 @@
 from pathlib import Path
+from langchain_text_splitters import MarkdownHeaderTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from app.shared.ai.client import get_ai_provider
 import pymupdf4llm
+import os
+
+ai = get_ai_provider()
 
 # need to change this to a macro or something where is the content folder is located
-folder = Path("/workspaces/1337-nexus/backend/content/subjects/pdfs")
+home = os.environ.get("HOME")
+print(f"Home directory: {home}")
+pdf_folder = Path(f"{home}/Desktop/1337-nexus/backend/content/subjects/pdfs")
+md_folder = Path(f"{home}/Desktop/1337-nexus/backend/content/subjects/mds")
+headers_to_split_on = [("#", "header 1"),
+                       ("##", "header 2"),
+                       ("###", "header 3")]
 
-for file in folder.glob("*.pdf"):
-    print(f"Processing {file.name}...")
+for file in pdf_folder.glob("*.pdf"):
+    md_file = md_folder / f"{file.stem}.md"
+    if md_file.exists():
+        print(f"Skipping {file.name}, already converted.")
+        continue
+    print(f"Converting {file.name} to Markdown...")
     mark = pymupdf4llm.to_markdown(file)
-    Path(f"/workspaces/1337-nexus/backend/content/subjects/mds/{file.stem}.md").write_text(mark)
+    md_file.write_text(mark)
+
+for md_file in md_folder.glob("*.md"):
+    print(f"Chunking {md_file.name}...")
+    mark = md_file.read_text()
+    splitter = MarkdownHeaderTextSplitter(headers_to_split_on=headers_to_split_on)
+    chunks = splitter.split_text(mark)
+    token_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100) # i can change it to [500,50]
+    final_chunks = token_splitter.split_documents(chunks)
+    print(ai.embed(final_chunks))
